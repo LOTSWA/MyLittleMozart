b6fcd1d816ac9c324bed86aa12aa0dd7bc6f2cf5
Stattius <xxblade6475xx@gmail.com>
2025-04-04 18:24:07 -0500

    committing before trying something

--- MyLittleMozartEB/src/mylittlemozart/strategy/ElectricBassGuitarStrategy.java
+++ MyLittleMozartEB/src/mylittlemozart/strategy/ElectricBassGuitarStrategy.java
@@ -1,5 +1,14 @@
 package mylittlemozart.strategy;
 
+import javax.sound.midi.*;
+
 public class ElectricBassGuitarStrategy implements InstrumentStrategy{
+	private static final int EBG = 33;
 	
+	
+	@Override
+	public void applyInstrument(Track track, int channel)
+	{
+		
+	}
 }
