94b793139b96d2e9502e8b518caaa81b0256c2f8
LOTSWA <55159891+LOTSWA@users.noreply.github.com>
2025-04-04 16:42:37 -0500

    parser

--- MyLittleMozartEB/src/mylittlemozart/information/MidiCsvParser.java
+++ MyLittleMozartEB/src/mylittlemozart/information/MidiCsvParser.java
@@ -1,10 +1,24 @@
 package mylittlemozart.information;
-
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 public class MidiCsvParser {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		try {
+			File CSV = new File("mystery_song.csv");
+			Scanner CSVReader = new Scanner(CSV);
+			while (CSVReader.hasNextLine()) {
+				String data = CSVReader.nextLine();
+				System.out.println(data);
+			}
+			CSVReader.close();
+		}
+		catch (FileNotFoundException e) {
+			System.out.println("an error occurred.");
+			e.printStackTrace();
+		}
 	}
 
 }
