3992e6013ac38f098976f08e21c3c91dbe06e42a
Stattius <xxblade6475xx@gmail.com>
2025-04-05 20:51:18 -0500

    started the comments and csv rework

--- MyLittleMozartEB/src/mylittlemozart/factory/LegatoMidiEventFactory.java
+++ MyLittleMozartEB/src/mylittlemozart/factory/LegatoMidiEventFactory.java
@@ -4,15 +4,30 @@ import javax.sound.midi.InvalidMidiDataException;
 import javax.sound.midi.MidiEvent;
 import javax.sound.midi.ShortMessage;
 
+/**
+ * factory for implementing the creation of legato Midi events
+ */
 public class LegatoMidiEventFactory implements MidiEventFactory
 {
+	/**
+	 * this method creates a note on midi event
+	 * @param tick
+	 * @param note
+	 * @param velocity
+	 * @param channel
+	 */
 	@Override
 	public MidiEvent createNoteOn(int tick, int note, int velocity, int channel) throws InvalidMidiDataException {
 		ShortMessage message = new ShortMessage();
 		message.setMessage(ShortMessage.NOTE_ON, channel, note, velocity);
 		return new MidiEvent(message, tick);
 	}
-	
+	/**
+	 * this method creates a note off legato midi event with a 80 tick delay
+	 * @param tick
+	 * @param note
+	 * @param channel	
+	 */
 	@Override
 	public MidiEvent createNoteOff(int tick, int note, int channel) throws InvalidMidiDataException {
 		int temp = tick + 80;
--- MyLittleMozartEB/src/mylittlemozart/factory/LegatoMidiEventFactoryAbstract.java
+++ MyLittleMozartEB/src/mylittlemozart/factory/LegatoMidiEventFactoryAbstract.java
@@ -1,7 +1,12 @@
 package mylittlemozart.factory;
-
+/**
+ * abstract factory that creates instances of legato midi event factories
+ */
 public class LegatoMidiEventFactoryAbstract implements MidiEventFactoryAbstract
 {
+	/**
+	 * 
+	 */
 	@Override
 	public MidiEventFactory createFactory() {
 	return new LegatoMidiEventFactory();
